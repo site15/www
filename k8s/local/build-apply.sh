5bdc3a6b0fd18c4b61d332282c42bc29a91efe4a
ILshat Khamitov <admin@site15.ru>
2020-07-29 00:23:43 +0500

    fix: update config name in build

--- k8s/local/build-apply.sh
+++ k8s/local/build-apply.sh
@@ -1,6 +1,6 @@
 export UID=$(id -u)
 export GID=$(id -g)
-docker-compose -f ./k8s/docker-compose.docker-compose-yml build
+docker-compose -f ./k8s/docker-compose.config-yml build
 docker save site15/server:local > ./tmp/site15-server.tar
 docker save site15/static:local > ./tmp/site15-static.tar
 microk8s ctr image import ./tmp/site15-server.tar
