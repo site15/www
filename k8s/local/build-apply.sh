8f6b9fb6ee9fb6196d3a7123284c80c3ff8da986
ILshat Khamitov <admin@site15.ru>
2020-07-29 00:33:45 +0500

    feat(k8s): update docker compose config

--- k8s/local/build-apply.sh
+++ k8s/local/build-apply.sh
@@ -1,8 +1,8 @@
 export UID=$(id -u)
 export GID=$(id -g)
-docker-compose -f ./k8s/docker-compose.config-yml build
-docker save site15/server:local > ./tmp/site15-server.tar
-docker save site15/static:local > ./tmp/site15-static.tar
-microk8s ctr image import ./tmp/site15-server.tar
-microk8s ctr image import ./tmp/site15-static.tar
+docker-compose -f ./k8s/local/docker-compose.config-yml build
+docker save site15/server:local > ./tmp/site15-server-local.tar
+docker save site15/static:local > ./tmp/site15-static-local.tar
+microk8s ctr image import ./tmp/site15-server-local.tar
+microk8s ctr image import ./tmp/site15-static-local.tar
 microk8s kubectl apply -f  ./k8s/local
