56a1a59d7c4c87e1457c0da56760699f75cddd8c
ILshat Khamitov <admin@site15.ru>
2020-07-31 00:30:55 +0500

    feat: update prod deploy config

--- k8s/local/apply.sh
+++ k8s/local/apply.sh
@@ -1,3 +1,3 @@
-microk8s kubectl delete deployment site15-static -n site15-prod --now
-microk8s kubectl delete deployment site15-server -n site15-prod --now
-microk8s kubectl apply -f  ./k8s/prod
+microk8s kubectl delete deployment site15-static -n site15-local --now
+microk8s kubectl delete deployment site15-server -n site15-local --now
+microk8s kubectl apply -f  ./k8s/local
