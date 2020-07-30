14829733a895d5a37e6eadd25f67aaec2075d457
ILshat Khamitov <admin@site15.ru>
2020-07-31 00:17:04 +0500

    feat: add prod deploy config

--- k8s/local/build-apply.sh
+++ k8s/local/build-apply.sh
@@ -1,8 +1,8 @@
 export UID=$(id -u)
 export GID=$(id -g)
-docker-compose -f ./k8s/local/docker-compose.config-yml build
-docker save site15/server:local > ./tmp/site15-server-local.tar
-docker save site15/static:local > ./tmp/site15-static-local.tar
-microk8s ctr image import ./tmp/site15-server-local.tar
-microk8s ctr image import ./tmp/site15-static-local.tar
-./k8s/local/apply.sh
+docker-compose -f ./k8s/prod/docker-compose.config-yml build
+docker save site15/server:prod > ./tmp/site15-server-prod.tar
+docker save site15/static:prod > ./tmp/site15-static-prod.tar
+microk8s ctr image import ./tmp/site15-server-prod.tar
+microk8s ctr image import ./tmp/site15-static-prod.tar
+./k8s/prod/apply.sh
