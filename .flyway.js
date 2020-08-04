97a2d5658cd47d94669d9a3b561018e8477868b4
ILshat Khamitov <admin@site15.ru>
2020-08-04 13:08:15 +0500

    feat(docker): add support work with db in dev mode

---
+++ .flyway.js
@@ -0,0 +1,53 @@
+const { ConnectionString } = require('connection-string');
+const connectionString = new ConnectionString(process.env.POSTGRES_URL);
+const USERNAME = connectionString.user;
+const PASSWORD = connectionString.password;
+const PORT = connectionString.hosts && +connectionString.hosts[0].port;
+const HOST = connectionString.hosts && connectionString.hosts[0].name;
+const DATABASE = connectionString.path && connectionString.path[0];
+const SCHEMA =
+  connectionString.params &&
+  connectionString.params &&
+  connectionString.params.schema;
+const SCHEMAS =
+  connectionString.params &&
+  connectionString.params &&
+  connectionString.params.schemas;
+
+module.exports = {
+  flywayArgs: {
+    url: `jdbc:postgresql://${HOST}:${PORT}/${DATABASE}`,
+    schemas: SCHEMAS || SCHEMA,
+    defaultSchema: SCHEMA,
+    locations: `filesystem:migrations`,
+    user: USERNAME,
+    password: PASSWORD,
+    table: '__migrations',
+    sqlMigrationSuffixes: '.pgsql',
+  },
+  // Use to configure environment variables used by flyway
+  env: {
+    JAVA_ARGS: '-Djava.util.logging.config.file=./conf/logging.properties',
+  },
+  version: '6.1.4', // optional, empty or missing will download the latest
+  mavinPlugins: [
+    {
+      // optional, use to add any plugins (ie. logging)
+      groupId: 'org.slf4j',
+      artifactId: 'slf4j-api',
+      version: '1.7.25',
+      // This can be a specifc url to download that may be different then the auto generated url.
+      downloadUrl:
+        'https://repo1.maven.org/maven2/org/slf4j/slf4j-api/1.7.25/slf4j-api-1.7.25.jar',
+    },
+    {
+      groupId: 'org.slf4j',
+      artifactId: 'slf4j-jdk14',
+      version: '1.7.25',
+    },
+  ],
+  downloads: {
+    storageDirectory: `${__dirname}/tmp`, // optional, the specific directory to store the flyway downloaded files. The directory must be writable by the node app process' user.
+    expirationTimeInMs: -1, // optional, -1 will never check for updates, defaults to 1 day.
+  },
+};
--- apps/site15-server/src/app/app.controller.ts
+++ apps/site15-server/src/app/app.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
-
+import { PrismaClient } from '@prisma/client';
 import { Message } from '@site15/common';
-
 import { AppService } from './app.service';
 
 @Controller()
@@ -11,5 +10,11 @@ export class AppController {
   @Get('hello')
   getData(): Message {
     return this.appService.getData();
+  }
+
+  @Get('db')
+  getDbData() {
+    const prisma = new PrismaClient();
+    return prisma.projectType.findMany({ select: { id: true, name: true } });
   }
 }
