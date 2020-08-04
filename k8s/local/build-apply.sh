e41458a3b61e6dcaac0f28705a4c9a93b40898e2
ILshat Khamitov <admin@site15.ru>
2020-08-05 01:28:42 +0500

    feat(k8s): add work with pg in local kuber

--- k8s/local/build-apply.sh
+++ k8s/local/build-apply.sh
@@ -1,6 +1,6 @@
 export UID=$(id -u)
 export GID=$(id -g)
-docker-compose -f ./k8s/local/docker-compose.config-yml build
+./docker/local/docker-compose-build.sh
 docker save site15/server:local > ./tmp/site15-server-local.tar
 docker save site15/static:local > ./tmp/site15-static-local.tar
 microk8s ctr image import ./tmp/site15-server-local.tar
