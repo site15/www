aa997c63863e375c8bce8cd24611e1fc6ab81f36
ILshat Khamitov <admin@site15.ru>
2020-08-05 13:41:20 +0500

    feat(k8s): update deploy

--- k8s/local/apply.sh
+++ k8s/local/apply.sh
@@ -1,8 +1,9 @@
-microk8s kubectl delete deployment site15-static -n site15-local --now
-microk8s kubectl delete deployment site15-server -n site15-local --now
-microk8s kubectl apply -f  ./k8s/local
+
 microk8s kubectl apply -f  ./k8s/local/postgres
 POD=$(microk8s kubectl get pod -l app=postgres -n postgres-local -o jsonpath="{.items[0].metadata.name}")
 microk8s kubectl exec -n postgres-local -ti $POD -- psql -U postgres -c "CREATE USER site15 WITH PASSWORD 'password'"
 microk8s kubectl exec -n postgres-local -ti $POD -- psql -U postgres -c "CREATE DATABASE site15"
 microk8s kubectl exec -n postgres-local -ti $POD -- psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE \"site15\" TO \"site15\""
+microk8s kubectl delete deployment site15-static -n site15-local --now
+microk8s kubectl delete deployment site15-server -n site15-local --now
+microk8s kubectl apply -f  ./k8s/local
--- k8s/prod/apply.sh
+++ k8s/prod/apply.sh
@@ -1,11 +1,11 @@
 ./k8s/prod/postgres/generator_1.configmap.yaml.sh
 ./k8s/prod/generator_global-service.yaml.sh
 ./k8s/prod/generator_1.configmap.yaml.sh
-/snap/bin/microk8s kubectl delete deployment site15-static -n site15-prod --now
-/snap/bin/microk8s kubectl delete deployment site15-server -n site15-prod --now
-/snap/bin/microk8s kubectl apply -f ./k8s/prod
 /snap/bin/microk8s kubectl apply -f  ./k8s/prod/postgres
 POD=$(/snap/bin/microk8s kubectl get pod -l app=postgres -n postgres-prod -o jsonpath="{.items[0].metadata.name}")
 /snap/bin/microk8s kubectl exec -n postgres-prod $POD -- psql -U postgres -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD'"
 /snap/bin/microk8s kubectl exec -n postgres-prod -ti $POD -- psql -U postgres -c "CREATE DATABASE site15"
 /snap/bin/microk8s kubectl exec -n postgres-prod -ti $POD -- psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE \"site15\" TO \"$POSTGRES_USER\""
+/snap/bin/microk8s kubectl delete deployment site15-static -n site15-prod --now
+/snap/bin/microk8s kubectl delete deployment site15-server -n site15-prod --now
+/snap/bin/microk8s kubectl apply -f ./k8s/prod
