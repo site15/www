e3d97434e360c605c7d40e45ae1d6cda7df2d852
ILshat Khamitov <admin@site15.ru>
2020-08-05 23:03:55 +0500

    feat(server): add compression

--- apps/site15-server/src/main.ts
+++ apps/site15-server/src/main.ts
@@ -7,10 +7,11 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
-
+import * as compression from 'compression';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+  app.use(compression);
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 5000;
   await app.listen(port, () => {
