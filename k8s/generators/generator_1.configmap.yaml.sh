4e3f92f674892ef639b3dec4f57f2a45e5b5b0a4
ILshat Khamitov <admin@site15.ru>
2020-08-08 14:43:45 +0500

    feat: add generator for k8s files

--- k8s/generators/generator_1.configmap.yaml.sh
+++
@@ -1,9 +0,0 @@
-cat <<EOF >./k8s/$HOST_TYPE/1.configmap.yaml
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  namespace: site15-$HOST_TYPE
-  name: site15-config
-data:
-  POSTGRES_URL: postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres.postgres-$HOST_TYPE:5432/site15?schema=public
-EOF
