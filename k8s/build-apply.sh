4c1c436984b733638c5922f802a08c8bb61ab9c4
ILshat Khamitov <admin@site15.ru>
2020-08-09 15:06:29 +0500

    fix: update deploy-prod

--- k8s/build-apply.sh
+++ k8s/build-apply.sh
@@ -1,5 +1,6 @@
 export UID=$(id -u)
 export GID=$(id -g)
+node ./k8s/generators/generator.js
 ./docker/$HOST_TYPE/docker-compose-build.sh
 docker save site15/server:$HOST_TYPE > ./tmp/site15-server-$HOST_TYPE.tar
 docker save site15/static:$HOST_TYPE > ./tmp/site15-static-$HOST_TYPE.tar
