a0b3c5bd4da1f60469746db2e4572bfa1ffe297e
ILshat Khamitov <admin@site15.ru>
2020-08-09 17:28:29 +0500

    feat: update deploy-prod

--- k8s/build-apply.sh
+++ k8s/build-apply.sh
@@ -2,9 +2,9 @@ export UID=$(id -u)
 export GID=$(id -g)
 node ./k8s/generators/generator.js
 ./docker/$HOST_TYPE/docker-compose-build.sh
-docker save site15/server:$HOST_TYPE > ./tmp/site15-server-$HOST_TYPE.tar
-docker save site15/static:$HOST_TYPE > ./tmp/site15-static-$HOST_TYPE.tar
-/snap/bin/microk8s ctr image import ./tmp/site15-server-$HOST_TYPE.tar
-/snap/bin/microk8s ctr image import ./tmp/site15-static-$HOST_TYPE.tar
+docker save $PROJECT_NAME/server:$HOST_TYPE > ./tmp/$PROJECT_NAME-server-$HOST_TYPE.tar
+docker save $PROJECT_NAME/static:$HOST_TYPE > ./tmp/$PROJECT_NAME-static-$HOST_TYPE.tar
+/snap/bin/microk8s ctr image import ./tmp/$PROJECT_NAME-server-$HOST_TYPE.tar
+/snap/bin/microk8s ctr image import ./tmp/$PROJECT_NAME-static-$HOST_TYPE.tar
 ./k8s/apply.sh
 /snap/bin/microk8s kubectl apply -f  ./k8s/$HOST_TYPE/postgres/services
