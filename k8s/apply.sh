1630ef0279b3acedda04e9b2c1d37b623d7a95a9
EndyKaufman <admin@site15.ru>
2020-09-07 22:08:11 +0400

    ci: 🎡 remove delete exists deployments on deploy

--- k8s/apply.sh
+++ k8s/apply.sh
@@ -7,6 +7,6 @@ POD=$(/snap/bin/microk8s kubectl get pod -l app=postgres -n postgres-$HOST_TYPE 
 /snap/bin/microk8s kubectl exec -n postgres-$HOST_TYPE -ti $POD -- psql -U postgres -d "$POSTGRES_DATABASE" -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $POSTGRES_USER"
 /snap/bin/microk8s kubectl exec -n postgres-$HOST_TYPE -ti $POD -- psql -U postgres -d "$POSTGRES_DATABASE" -c "GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DATABASE TO $POSTGRES_USER"
 /snap/bin/microk8s kubectl exec -n postgres-$HOST_TYPE -ti $POD -- psql -U postgres -d "$POSTGRES_DATABASE" -c "ALTER USER $POSTGRES_USER WITH LOGIN CREATEROLE NOCREATEDB NOSUPERUSER INHERIT"
-/snap/bin/microk8s kubectl delete deployment $PROJECT_NAME-frontend -n $PROJECT_NAME-$HOST_TYPE --now
-/snap/bin/microk8s kubectl delete deployment $PROJECT_NAME-backend -n $PROJECT_NAME-$HOST_TYPE --now
+# /snap/bin/microk8s kubectl delete deployment $PROJECT_NAME-frontend -n $PROJECT_NAME-$HOST_TYPE --now
+# /snap/bin/microk8s kubectl delete deployment $PROJECT_NAME-backend -n $PROJECT_NAME-$HOST_TYPE --now
 /snap/bin/microk8s kubectl apply -f ./k8s/$HOST_TYPE
