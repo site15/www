72d99b0aa1b7b1cb2040d230235f29ea5d0262dd
EndyKaufman <admin@site15.ru>
2021-03-12 16:06:26 +0400

    fix: add wait ready pg database

--- k8s/apply.sh
+++ k8s/apply.sh
@@ -1,4 +1,5 @@
 /snap/bin/microk8s kubectl apply -f  ./k8s/$HOST_TYPE/postgres
+/snap/bin/microk8s kubectl wait --for=condition=ready --timeout=180s pod -n postgres-$HOST_TYPE -l app=postgres
 POD=$(/snap/bin/microk8s kubectl get pod -l app=postgres -n postgres-$HOST_TYPE -o jsonpath="{.items[0].metadata.name}")
 /snap/bin/microk8s kubectl exec -n postgres-$HOST_TYPE $POD -- psql -U postgres -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD'"
 /snap/bin/microk8s kubectl exec -n postgres-$HOST_TYPE -ti $POD -- psql -U postgres -c "CREATE DATABASE $POSTGRES_DATABASE"
